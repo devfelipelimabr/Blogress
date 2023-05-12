09802ea75daffd5ba38f2afde07d245abebc8531
devfelipelimabr <felipelima85@hotmail.com>
2023-05-12 10:02:30 -0300

    category registration inserted & slugify.

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const slugify = require("slugify");
 const router = express.Router();
+const Category = require("./Category");
 
 router.get("/admin/categories/new", (req, res) => {
-  res.render("admin/categories/new")
+  res.render("admin/categories/new");
 });
 
+router.post("/categories/save", (req, res) => {
+  const title = req.body.title;
+  if (title != undefined) {
+    Category.create({
+      title: title,
+      slug: slugify(title),
+    }).then(() => {
+      res.redirect("/");
+    });
+  } else {
+    res.redirect("admin/categories/new");
+  }
+});
+
+router.get("/admin/categories", (req,res)=>{
+  
+})
 
 module.exports = router;
