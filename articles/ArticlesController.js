7526f62f3aee744620cc074b250f093e7c90b758
devfelipelimabr <felipelima85@hotmail.com>
2023-05-12 23:18:17 -0300

    Created article form.

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const slugify = require("slugify");
 const router = express.Router();
+const Article = require("./Article");
 
 router.get("/articles", (req, res) => {
   res.send("ROTA DE ARTIGOS");
 });
 
 router.get("/admin/articles/new", (req, res) => {
-  res.send("CRIAR NOVO ARTIGO");
+  res.render("admin/articles/new");
+});
+
+router.post("/articles/save", (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  if ((title && body) != undefined) {
+    Article.create({
+      title: title,
+      slug: slugify(title),
+      body: body,
+    }).then(() => {
+      res.redirect("/admin/articles");
+    });
+  } else {
+    res.redirect("admin/articles/new");
+  }
 });
 
 module.exports = router;
