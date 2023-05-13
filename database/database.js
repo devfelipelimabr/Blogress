86ff63e1bf0eea8ef02626b41eccb8f4ce73cc1e
devfelipelimabr <felipelima85@hotmail.com>
2023-05-13 00:32:31 -0300

    Changed timezone for UTC -03h.

--- database/database.js
+++ database/database.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize("blogress", "root", "128145", {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00"
 });
 
 module.exports = connection;
