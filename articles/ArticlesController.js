88c7320c5054526bbeb5362526f5451154763d20
devfelipelimabr <felipelima85@hotmail.com>
2023-05-13 00:04:48 -0300

    Created NEW ARTICLE route.

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -2,23 +2,28 @@ const express = require("express");
 const slugify = require("slugify");
 const router = express.Router();
 const Article = require("./Article");
+const Category = require("../categories/Category");
 
 router.get("/articles", (req, res) => {
   res.send("ROTA DE ARTIGOS");
 });
 
 router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new");
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories: categories });
+  });
 });
 
 router.post("/articles/save", (req, res) => {
   const title = req.body.title;
   const body = req.body.body;
-  if ((title && body) != undefined) {
+  const categoryId = req.body.category;
+  if (title != undefined && body != undefined) {
     Article.create({
       title: title,
       slug: slugify(title),
       body: body,
+      categoryId: categoryId,
     }).then(() => {
       res.redirect("/admin/articles");
     });
