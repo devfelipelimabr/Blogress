2f3fc733b61ebdcd9b8eb530ab718ddef73e24a5
devfelipelimabr <felipelima85@hotmail.com>
2023-05-13 01:44:57 -0300

    Inserted update article database CRUD.

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -51,4 +51,47 @@ router.post("/articles/delete", (req, res) => {
   }
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (isNaN(id)) {
+    res.redirect("/admin/articles");
+  }
+
+  Article.findByPk(id)
+    .then((article) => {
+      if (article != undefined) {
+        Category.findAll().then((categories) => {
+          res.render("admin/articles/edit", {
+            article: article,
+            categories: categories,
+          });
+        });
+      } else {
+        res.redirect("/admin/articles");
+      }
+    })
+    .catch((erro) => {
+      res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/update", (req, res) => {
+  const id = req.body.id;
+  const title = req.body.title;
+  const body = req.body.body;
+  const categoryId = req.body.category;
+
+  Article.update(
+    { title: title, slug: slugify(title), body: body, categoryId: categoryId },
+    {
+      where: {
+        id: id,
+      },
+    }
+  ).then(() => {
+    res.redirect("/admin/articles");
+  });
+});
+
 module.exports = router;
